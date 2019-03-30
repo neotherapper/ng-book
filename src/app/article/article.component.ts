b769ad8c26c0b7779491d1165e59a7a981228680
GEORGE PILITSOGLOU <george.pilitsoglou@gmail.com>
2019-03-30 17:52:20 +0000

    angular-reddit: using voteUp voteDown component functions

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -20,4 +20,15 @@ export class ArticleComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  voteUp(): boolean {
+    this.article.voteUp();
+    return false;
+  }
+
+  voteDown(): boolean {
+    this.article.voteUp();
+    return false;
+  }
+
 }
