dd3ac579022ec21039263f691a31b5dbe407f841
GEORGE PILITSOGLOU <george.pilitsoglou@gmail.com>
2019-03-30 17:31:32 +0000

    angular-reddit added article component

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.scss']
+})
+export class ArticleComponent implements OnInit {
+  @HostBinding('attrs.class') cssClass = 'row';
+  votes: number;
+  title: string;
+  link: string;
+
+  constructor() {
+    this.title = 'Neotherapper';
+    this.link = 'http://angular.io';
+    this.votes = 10;
+  }
+
+  ngOnInit() {
+  }
+
+  voteUp() {
+    this.votes += 1;
+  }
+
+  voteDown() {
+    this.votes -= 1;
+  }
+
+}
